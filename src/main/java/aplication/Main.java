ed1c24de9e81aa1d91e093bc84d3c966d4704575
Lucas Felipe Felicio Santos <lucasfelicio378@gmail.com>
2023-06-27 22:10:13 -0300

    adicionando teste no main para login e cadastro

--- src/main/java/aplication/Main.java
+++ src/main/java/aplication/Main.java
@@ -1,7 +1,75 @@
 package aplication;
 
+import dao.UsuarioDAO;
+import model.Credencial;
+import model.Usuario;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Criação do objeto UsuarioDAO
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+        // Criação do objeto Scanner para leitura de entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        int opcao;
+        do {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Login");
+            System.out.println("2 - Cadastrar");
+            System.out.println("0 - Fechar");
+            opcao = scanner.nextInt();
+            scanner.nextLine(); // Limpar o buffer de leitura
+
+            switch (opcao) {
+                case 1:
+                    // Solicitar dados para o login
+                    System.out.println("Digite o email para login:");
+                    String email = scanner.nextLine();
+
+                    System.out.println("Digite a senha para login:");
+                    String senha = scanner.nextLine();
+
+                    // Realizar o login
+                    boolean loginSucesso = usuarioDAO.realizarLogin(email, senha);
+                    if (!loginSucesso) {
+                        System.out.println("Deseja tentar novamente? (S/N)");
+                        String resposta = scanner.nextLine();
+                        if (!resposta.equalsIgnoreCase("S")) {
+                            opcao = 0; // Fechar o programa
+                        }
+                    }
+                    break;
+                case 2:
+                    // Solicitar dados para o cadastro de usuário
+                    System.out.println("Digite o nome:");
+                    String nome = scanner.nextLine();
+
+                    System.out.println("Digite o CPF:");
+                    long cpf = scanner.nextLong();
+                    scanner.nextLine(); // Limpar o buffer de leitura
+
+                    System.out.println("Digite o email:");
+                    email = scanner.nextLine();
+
+                    System.out.println("Digite a senha:");
+                    senha = scanner.nextLine();
+
+                    // Cadastro do usuário
+                    usuarioDAO.cadastrarUsuario(nome, cpf, email, senha);
+
+                    break;
+                case 0:
+                    System.out.println("Fechando o programa...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Por favor, escolha uma opção válida.");
+            }
+        } while (opcao != 0);
+
+        // Fechar o scanner
+        scanner.close();
     }
 }
--- src/main/java/util/QuerySQL.java
+++ src/main/java/util/QuerySQL.java
@@ -5,9 +5,9 @@ public class QuerySQL {
 
     public static final String INSERIR_USUARIO_COM_ENDERECO = "INSERT INTO Usuario (nome, cpf, idCredencial, idEndereco) VALUES (?, ?, ?, ?)";
 
-    public static final String COUNT_USUARIO_POR_EMAIL = "SELECT COUNT(*) FROM Usuario WHERE email = ?";
+    public static final String COUNT_USUARIO_POR_EMAIL = "SELECT COUNT(*) FROM Credencial WHERE email = ?";
 
-    public static final String COUNT_USUARIO_POR_EMAIL_SENHA = "SELECT COUNT(*) FROM Usuario WHERE email = ? AND senha = ?";
+    public static final String COUNT_USUARIO_POR_EMAIL_SENHA = "SELECT COUNT(*) FROM Credencial WHERE email = ? AND senha = ?";
 
     public static final  String INSERIR_CREDENCIAL = "INSERT INTO Credencial (idCredencial, email, senha, ativo) VALUES (?, ?, ?, ?)";
 }
